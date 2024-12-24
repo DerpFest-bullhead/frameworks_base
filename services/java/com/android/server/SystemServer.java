LMODroid-TMP/platform_frameworks_base

base: Include lmodroid framework
Change-Id: Ia364219e450a2ddf05bf71ae8efe76b977a747c5

services/java/com/android/server/SystemServer.java
@@ -249,6 +249,8 @@ import java.util.concurrent.Future;
 // LineageHardware
 import com.android.server.libremobileos.LineageHardwareService;
 
+import com.libremobileos.server.LMODroidSystemServer;
+
 /**
  * Entry point to {@code system_server}.
  */
@@ -2737,6 +2739,10 @@ public final class SystemServer implements Dumpable {
         // Perfetto TracingServiceProxy
         t.traceBegin("startTracingServiceProxy");
         mSystemServiceManager.startService(TracingServiceProxy.class);
+        t.traceEnd();
+
+        t.traceBegin("startLMODroidServices");
+        LMODroidSystemServer.startServices(context, mSystemServiceManager);
         t.traceEnd();
 
         // It is now time to start up the app processes...

DerpFest-bullhead/frameworks_base

services/java/com/android/server/SystemServer.java
@@ -311,6 +311,8 @@ import java.util.TreeSet;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 
+import com.libremobileos.server.LMOSystemServer;
+
 /**
  * Entry point to {@code system_server}.
  */
@@ -2870,6 +2872,10 @@ public final class SystemServer implements Dumpable {
         // Perfetto TracingServiceProxy
         t.traceBegin("startTracingServiceProxy");
         mSystemServiceManager.startService(TracingServiceProxy.class);
+        t.traceEnd();
+
+        t.traceBegin("startLMODroidServices");
+        LMOSystemServer.startServices(context, mSystemServiceManager);
         t.traceEnd();
 
         // It is now time to start up the app processes...